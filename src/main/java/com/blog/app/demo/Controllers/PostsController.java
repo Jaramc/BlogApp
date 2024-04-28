e30ab2dc0d226b2b94df1a0a076b3fdb9b0aac88
Ares <168306808+Ximejaca@users.noreply.github.com>
2024-04-28 01:19:32 -0500

    add: post management user verification

--- src/main/java/com/blog/app/demo/Controllers/PostsController.java
+++ src/main/java/com/blog/app/demo/Controllers/PostsController.java
@@ -1,25 +1,44 @@
 package com.blog.app.demo.Controllers;
 
-import com.blog.app.demo.Models.Post;
 import com.blog.app.demo.Requests.PostRequest;
 import com.blog.app.demo.Responses.PostResponse;
 import com.blog.app.demo.Services.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class PostsController {
     private final PostService postService;
-    @PostMapping(value = "index")
-    public String getPosts(){
-        return "Hello World from posts";
+
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        List<PostResponse> responses = postService.getAllPosts();
+        return ResponseEntity.ok(responses);
     }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostResponse> getPost(@PathVariable Long postId) {
+        PostResponse response = postService.getPost(postId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/update/{postId}")
+    public ResponseEntity<PostResponse> updatePost(@PathVariable Long postId, @RequestBody PostRequest request) {
+        PostResponse response = postService.updatePost(postId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete/{postId}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        postService.deletePost(postId);
+        return ResponseEntity.noContent().build();
+    }
+
     @PostMapping("/create")
     public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest request) {
         PostResponse response = postService.createPost(request);
--- src/main/java/com/blog/app/demo/Services/PostService.java
+++ src/main/java/com/blog/app/demo/Services/PostService.java
@@ -12,14 +12,69 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
 
+
+    public PostResponse getPost(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("Post not found"));
+        return buildPostResponse(post);
+    }
+
+    public List<PostResponse> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(this::buildPostResponse).collect(Collectors.toList());
+    }
+
+    public PostResponse updatePost(Long postId, PostRequest request) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new RuntimeException("Post not found"));
+        String currentUsername = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        if (!post.getUser().getUsername().equals(currentUsername)) {
+            throw new RuntimeException("Unauthorized attempt to edit a post");
+        }
+        post.setTitle(request.getTitle());
+        post.setContent(request.getContent());
+        post = postRepository.save(post);
+
+        return buildPostResponse(post);
+    }
+
+    public void deletePost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new RuntimeException("Post not found"));
+        String currentUsername = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        if (!post.getUser().getUsername().equals(currentUsername)) {
+            throw new RuntimeException("Unauthorized attempt to delete a post");
+        }
+        postRepository.delete(post);
+    }
+
+    private PostResponse buildPostResponse(Post post) {
+        User user = post.getUser();
+        UserResponse userResponse = UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .build();
+
+        return PostResponse.builder()
+                .id(post.getId().intValue())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .user(userResponse)
+                .build();
+    }
+
     public PostResponse createPost(PostRequest request) {
-        String username = ((UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+        String username = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
         User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
 
         Post post = Post.builder()
